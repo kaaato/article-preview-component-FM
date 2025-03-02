301571200165c4ad35839e85b6838e03a877ab5d
kaaato <kaaato@gmail.com>
2025-03-02 15:43:02 +0100

    modify html structure and corresponding css and js. Instead of creating new share-link div and tooltip every click, use a css class with display: none and toggle it differently depends on viewport size when clicked

--- main.js
+++ main.js
@@ -1,106 +1,60 @@
 "use strict";
 
+let authorDiv = document.querySelector(".author");
 let authorButton = document.querySelector(".author__button");
-let tooltip;
+let shareDiv = document.querySelector(".share-links");
+let shareButton = document.querySelector(".share-links__button");
+let list = document.querySelector(".list");
+let text = document.querySelector(".text")
+let isTooltip;
 
-authorButton.addEventListener("click", handleClickOnAuthorButton);
+authorButton.addEventListener("click", handleClickOnButton);
+shareButton.addEventListener("click", handleClickOnButton);
 window.onresize = handleResize;
 
-function handleClickOnAuthorButton(event) {
+function handleClickOnButton(event) {
   let windowWidth = document.documentElement.clientWidth;
-  
-  if (tooltip) return;
 
-  if (windowWidth < 640) { // viewport max-width 640px
+  if (windowWidth < 640) {
     switchFooter();
-  } else { // viewport min-width 640px
+  } else {
     let tooltipWidth = 250;
-    let authorButtonCoords = authorButton.getBoundingClientRect();
-    let isEnoughSpace = (tooltipWidth / 2) < (windowWidth - authorButtonCoords.left - authorButton.offsetWidth / 2);
+    let buttonCoords = this.getBoundingClientRect();
+    let isEnoughSpace = (tooltipWidth / 2) < (windowWidth - buttonCoords.left - this.offsetWidth / 2);
 
-    if (isEnoughSpace) {
-      tooltip = createTooltip();
-      showTooltip(tooltip, authorButtonCoords);
-      authorButton.addEventListener("click", removeTooltip);
+    if (isEnoughSpace && this == authorButton) {
+      if (isTooltip) return;
+      isTooltip = true;
+      showTooltip();
+      this.addEventListener("click", removeTooltip);
     } else {
       switchFooter();
     }
   }
 }
 
-function handleResize(event) {
-  if (!tooltip) return;
-
-  removeTooltip();
-}
-
-function removeTooltip(event) {
-  tooltip.remove();
-  authorButton.removeEventListener("click", removeTooltip);
-  tooltip = null;
-}
-
-function showTooltip(tooltip, buttonCoords) {
-  document.body.append(tooltip);
-  let tooltipX = buttonCoords.left - ((tooltip.offsetWidth - authorButton.offsetWidth) / 2 );
-  let textElement = document.querySelector(".text");
-  let tooltipY = textElement.getBoundingClientRect().top;
-
-  tooltip.style.left = tooltipX + "px";
-  tooltip.style.top = tooltipY + "px";
-}
-
-function createTooltip() {
-  let div = document.createElement("div");
-  div.classList.add("tooltip");
-  div.insertAdjacentHTML(
-    "beforeend",
-    `
-      <div class="tooltip__contents">            
-        <p class="share-links__text">Share</p>
-        <ul class="tooltip__list list">
-          <li><a href="#"><img src="./images/icon-facebook.svg" alt="A link to Facebook" width="20" height="20" class="link-img"/></a></li>
-          <li><a href="#"><img src="./images/icon-twitter.svg" alt="A link to Twitter" width="20" height="20" class="link-img"/></a></li>
-          <li><a href="#"><img src="./images/icon-pinterest.svg" alt="A link to Pinterest" width="20" height="20" class="link-img"/></a></li>
-        </ul></div>
-      <div class="triangle-container"><span class="triangle"></span></div>     
-    `
-  )
-  return div;
-}
-
 function switchFooter() {
-  let shareLinkDiv = createShareLinkElement();
-  let footer = document.querySelector(".footer-container");
-  footer.append(shareLinkDiv);
-  let shareLinkButton = document.querySelector(".share-links__button");
-  shareLinkButton.addEventListener('click', handleClickOnShareLinkButton);
+  authorDiv.classList.toggle("hidden");
+  shareDiv.classList.toggle("hidden");
 }
 
-function handleClickOnShareLinkButton(event) {
-  let shareLinkDiv = document.querySelector(".share-links");
-  let authorDiv = document.querySelector(".author")
-  document.querySelector(".share-links__button").removeEventListener("click", handleClickOnShareLinkButton);
-  shareLinkDiv.replaceWith(authorDiv);
-  shareLinkDiv = null;
+function showTooltip() {
+  shareDiv.className = "tooltip";
+  list.classList.toggle("list--tooltip");
+  shareButton.classList.toggle("hidden");
+  shareDiv.style.top = text.getBoundingClientRect().top + "px";
+  shareDiv.style.left = authorButton.getBoundingClientRect().left - ((shareDiv.offsetWidth - authorButton.offsetWidth) / 2) + "px";
 }
 
-function createShareLinkElement() {
-  let div = document.createElement("div");
-  div.classList.add("share-links", "footer");
-  div.insertAdjacentHTML(
-    "beforeend",   
-    ` 
-      <p class="share-links__text">Share</p>
-      <ul class="list">
-        <li><a href="#"><img src="./images/icon-facebook.svg" alt="A link to Facebook" width="20" height="20" class="link-img"/></a></li>
-        <li><a href="#"><img src="./images/icon-twitter.svg" alt="A link to Twitter" width="20" height="20" class="link-img"/></a></li>
-        <li><a href="#"><img src="./images/icon-pinterest.svg" alt="A link to Pinterest" width="20" height="20" class="link-img"/></a></li>
-      </ul>
-      <button type="button" class="share-links__button button">
-        <img src="./images/icon-share.svg" alt="share links" width="32" height="32" class="button__img button__img--dark" />
-      </button>      
-    `
-  )
-  return div;
+function removeTooltip() {
+  shareDiv.className = "share-links footer hidden";
+  list.classList.toggle("list--tooltip");
+  shareButton.classList.toggle("hidden");
+  authorButton.removeEventListener("click", removeTooltip);
+  isTooltip = null;
+}
+
+function handleResize() {
+  if (!isTooltip) return;
+  removeTooltip();
 }
